cb4c4d617995d8b6914991cb7cd09075fd84a148
Dave G <Dgonsalves22@gmail.com>
2023-04-29 20:47:36 -0500

    clj-new template build

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to terrable
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/shc/terrable_test.clj
@@ -0,0 +1,7 @@
+(ns shc.terrable-test
+  (:require [clojure.test :refer :all]
+            [shc.terrable :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
